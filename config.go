201bf48722fe5165f916f1569d4069c1af81ba4e
Heorhi Valakhanovich <code@mail.geov.name>
2023-03-21 17:26:18 +0300

    refactor config parsing

---
+++ config.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+var defaultConfigData = `
+listen_url = "0.0.0.0:6666"
+#secret = "dd000102030405060708090a0b0c0d0e0f"
+socks5 = "127.0.0.1:9050"
+[users]
+1 = "dd000102030405060708090a0b0c0d0e0f"
+2 = "dd101112131415161718191a1b1c1d1e1f"
+`
+
+type Config struct {
+	Listen_Url string
+	Secret     *string
+	Socks5     *string
+	Users      *map[string]string
+}
+
+func readConfig(path string) (*Config, error) {
+	var configData []byte
+	var config Config
+	configData, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		return nil, err
+	}
+	_, err = toml.Decode(string(configData), &config)
+	if err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
+
+func defaultConfig() *Config {
+	var config Config
+	_, err := toml.Decode(defaultConfigData, &config)
+	if err != nil {
+		panic(err)
+	}
+	return &config
+}
--- main.go
+++ main.go
@@ -2,28 +2,9 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net"
 	"os"
-
-	"github.com/BurntSushi/toml"
 )
-
-var defaultConfig = `
-listen_url = "0.0.0.0:6666"
-#secret = "dd000102030405060708090a0b0c0d0e0f"
-socks5 = "127.0.0.1:9050"
-[users]
-1 = "dd000102030405060708090a0b0c0d0e0f"
-2 = "dd101112131415161718191a1b1c1d1e1f"
-`
-
-type Config struct {
-	Listen_Url string
-	Secret     *string
-	Socks5     *string
-	Users      *map[string]string
-}
 
 func handleConnection(conn net.Conn, dcConn DCConnector, userDB *Users) {
 	handleObfuscated(conn, dcConn, userDB)
@@ -40,25 +21,17 @@ func listenForConnections(listener net.Listener, dcc DCConnector, userDB *Users)
 }
 
 func main() {
-	var configData []byte
+	var c *Config
 	if len(os.Args) > 1 {
 		var err error
-		configData, err = ioutil.ReadFile(os.Args[1])
+		c, err = readConfig(os.Args[1])
 		if err != nil {
 			panic(err)
 		}
 	} else {
-		configData = []byte(defaultConfig)
-	}
-	c := &Config{}
-	_, err := toml.Decode(string(configData), &c)
-	if err != nil {
-		panic(err)
+		c = defaultConfig()
 	}
 	fmt.Printf("listen: %s\n", c.Listen_Url)
-	if err != nil {
-		panic(err)
-	}
 	listener, err := net.Listen("tcp", c.Listen_Url)
 	if err != nil {
 		panic(err)
