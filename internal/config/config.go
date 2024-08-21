18061323df0e7d1be9b2f5596cd07e6aeec93ab0
Heorhi Valakhanovich <code@mail.geov.name>
2024-08-21 16:53:32 +0300

    fix iterator in config

--- internal/config/config.go
+++ internal/config/config.go
@@ -114,7 +114,7 @@ func (c *Config) GetUserSecret(user string) (string, error) {
 func (c *Config) IterateUsers() func(func(*User) bool) {
 	return func(fn func(*User) bool) {
 		for _, u := range c.users.Users {
-			if fn(u) {
+			if !fn(u) {
 				return
 			}
 		}
